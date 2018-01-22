acbdc6296944a08292f0df433b2de5b9e1f5a39a
soonboylena <17131007@qq.com>
2018-01-22 23:10:25 +0800

    分包

---
+++ browser/src/main/java/com/github/soonboylena/security/browser/BrowserApplication.java
@@ -0,0 +1,12 @@
+package com.github.soonboylena.security.browser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrowserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrowserApplication.class, args);
+	}
+}
---
+++ demo/src/main/java/com/github/soonboylena/security/demo/SecurityDemoApplication.java
@@ -0,0 +1,12 @@
+package com.github.soonboylena.security.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityDemoApplication.class, args);
+	}
+}
