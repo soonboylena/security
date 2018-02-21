263280eced60226c62f4d4259c8f3dc5daad6c3b
sunbo <17131007@qq.com>
2018-02-21 22:07:43 +0800

    1

--- core/src/main/java/com/github/soonboylena/security/core/properties/BrowserProperty.java
+++ core/src/main/java/com/github/soonboylena/security/core/properties/BrowserProperty.java
@@ -1,8 +1,10 @@
 package com.github.soonboylena.security.core.properties;
 
 import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @Data
+@ConfigurationProperties(prefix = "browser")
 public class BrowserProperty {
-    private String loginPage = "";
+    private String loginPage = "/aoo";
 }
